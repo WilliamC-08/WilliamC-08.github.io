13f1b650ae591b7c03705ae72503d61fc91ac093
WilliamC-08 <71800795+WilliamC-08@users.noreply.github.com>
2021-02-21 00:18:53 +0800

    Update kop.js

--- KingOfPopF/assets/js/kop.js
+++ KingOfPopF/assets/js/kop.js
@@ -1,4 +1,4 @@
-const endpoint_url = 'https://srv4.pweb201.local/KingOfPop/api/';
+const endpoint_url = 'https://https://51018021.p-web.click/KingOfPop/api/';
 
 //newline to br
 /*function nl2br (str, is_xhtml) {
